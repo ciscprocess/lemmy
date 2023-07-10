e03e40473415af3bae5907e36a650a0e2cd60abf
Nathan Korzekwa <ciscprocess@gmail.com>
2023-07-09 20:54:03 -0400

    test

--- docker/build_arm_build_base.sh
+++ docker/build_arm_build_base.sh
@@ -3,12 +3,14 @@ set -e
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
-sudo docker build . -f Dockerfile.base-arm -t "dessalines/muslrust:1.0.0" --platform=linux/arm64
+TAG=dessalines/musl-rust-builder:latest
+
+sudo docker build . -f Dockerfile.base-arm -t "$TAG" --platform=linux/arm64
 
 pushd ..
 sudo docker build . \
     -f docker/Dockerfile \
-    --build-arg BASE_IMAGE="dessalines/muslrust:1.0.0" \
+    --build-arg BASE_IMAGE="dessalines/musl-rust-builder:latest" \
     --build-arg CARGO_BUILD_TARGET=aarch64-unknown-linux-musl \
     --build-arg RUST_RELEASE_MODE=debug \
     -t "dessalines/lemmy:dev" \
